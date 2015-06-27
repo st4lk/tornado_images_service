c866318cde83628d6fcbbe25f99c14770498442f
st4lk <myhappydo@gmail.com>
2015-06-28 00:57:58 +0300

    don't set default json header

--- service/apps/base/mixins.py
+++ service/apps/base/mixins.py
@@ -2,9 +2,6 @@ from tornado.escape import json_encode
 
 
 class JSONResponseMixin(object):
-    def set_default_headers(self):
-        super(JSONResponseMixin, self).set_default_headers()
-        self.set_header("Content-Type", "application/json; charset=UTF-8")
-
     def respond_json(self, data, status=200):
+        self.set_header("Content-Type", "application/json; charset=UTF-8")
         self.write(json_encode(data))
